10bebbb0a4d5b615960894f1df93ccaf594596b9
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-03-20 20:06:23 -0300

    create Campaign reoutes

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,34 @@
 import { Router } from "express";
 import { LeadsController } from "./controllers/LeadsController";
 import { GroupsController } from "./controllers/GroupsController";
+import { CampaignsController } from "./controllers/CampaignsController";
 
 const router = Router();
 
 const leadsController = new LeadsController()
 const groupsController = new GroupsController()
+const campaignsController = new CampaignsController()
 
+// Leads
 router.get("/leads", leadsController.index)
 router.post("/leads", leadsController.create)
 router.get("/leads/:id", leadsController.show)
 router.put("/leads/:id", leadsController.update)
 router.delete("/leads/:id", leadsController.delete)
 
+// Groups
 router.get("/groups", groupsController.index)
 router.post("/groups", groupsController.create)
 router.get("/groups/:id", groupsController.show)
 router.put("/groups/:id", groupsController.update)
 router.delete("/groups/:id", groupsController.delete)
+
+// Campaigns
+router.get("/campaigns", campaignsController.index)
+router.post("/campaigns", campaignsController.create)
+router.get("/campaigns/:id", campaignsController.show)
+router.put("/campaigns/:id", campaignsController.update)
+router.delete("/campaigns/:id", campaignsController.delete)
 
 router.get("/status", async (req, res, next) => {
   try {
