01232299f44ba422cb1f6a33f29e808205be6e59
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-03-20 11:48:00 -0300

    Implements leads routes: index, create, show, update and delete.

---
+++ src/controllers/LeadsController.ts
@@ -0,0 +1,76 @@
+import { Handler } from "express";
+import { prisma } from "../database";
+import { CreateLeadRequestSchema, UpdateLeadRequestSchema } from "./schemas/LeadsRequestSchema";
+import { HttpError } from "../errors/HttpError";
+
+export class LeadsController {
+  index: Handler = async (req, res, next) => {
+    try {
+      const leads = await prisma.lead.findMany()
+      res.json(leads)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  create: Handler = async (req, res, next) => {
+    try {
+      const body = CreateLeadRequestSchema.parse(req.body)
+      const newLead = await prisma.lead.create({
+        data: body
+      })
+      res.status(201).json(newLead)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  show: Handler = async (req, res, next) => {
+    try {
+      const lead = await prisma.lead.findUnique({
+        where: { id: +req.params.id },
+        include: {
+          groups: true,
+          campaigns: true
+        }
+      })
+
+      if (!lead) throw new HttpError(404, "Lead not found!")
+
+      res.json(lead)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  update: Handler = async (req, res, next) => {
+    try {
+      const id = Number(req.params.id)
+      const body = UpdateLeadRequestSchema.parse(req.body)
+
+      const leadExists = await prisma.lead.findUnique({ where: { id } })
+      if (!leadExists) throw new HttpError(404, "Leads not found!")
+
+      const updatedLead = await prisma.lead.update({ data: body, where: { id } })
+
+      res.json(updatedLead)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  delete: Handler = async (req, res, next) => {
+    try {
+      const id = Number(req.params.id)
+
+      const leadExists = await prisma.lead.findUnique({ where: { id } })
+      if (!leadExists) throw new HttpError(404, "Leads not found!")
+      
+      const deletedLead = await prisma.lead.delete({ where: { id } })
+
+      res.json({ deletedLead })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,15 @@
 import { Router } from "express";
+import { LeadsController } from "./controllers/LeadsController";
 
 const router = Router();
+
+const leadsController = new LeadsController()
+
+router.get("/leads", leadsController.index)
+router.post("/leads", leadsController.create)
+router.get("/leads/:id", leadsController.show)
+router.put("/leads/:id", leadsController.update)
+router.delete("/leads/:id", leadsController.delete)
 
 router.get("/status", async (req, res, next) => {
   try {
